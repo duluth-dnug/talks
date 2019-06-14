8280736da9cbedfbc5f3b4245af848020e601125
Adam Chevalier <chevalierAdam2@gmail.com>
2019-06-14 08:26:35 -0500

    Selenium from the Ground Up (#7)
    * add SeleniumGroundUp talk
    
    * remove unnecessary files

---
+++ 2019/SeleniumGroundUp/SeleniumDemo/PageClasses.cs
@@ -0,0 +1,54 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+
+namespace SeleniumDemo
+{
+    public class GithubHome : Page
+    {
+        private RemoteWebDriver _driver;
+
+        public GithubHome(RemoteWebDriver driver) : base(driver) { }
+
+        public SearchResults Search(string query)
+        {
+            _driver.FindElement(By.Name("q")).SendKeys("quake" + Keys.Enter);
+
+            // smart wait of some kind
+
+            return new SearchResults(_driver);
+        }
+    }
+
+    public class SearchResults : Page
+    {
+        public SearchResults(RemoteWebDriver driver) : base(driver) { }
+
+        public RepositoryPage ClickResult(int index)
+        {
+            _driver.FindElement(By.ClassName("repo-list"))
+                .FindElements(By.TagName("a"))[index].Click();
+
+            return new RepositoryPage(_driver);
+        }
+    }
+
+    public class RepositoryPage : Page
+    {
+        public RepositoryPage(RemoteWebDriver driver) : base(driver) { }
+    }
+
+    public class Page
+    {
+        protected RemoteWebDriver _driver;
+
+        public Page(RemoteWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        protected void NavigateTo(string url)
+        {
+            _driver.Navigate().GoToUrl(url);
+        }
+    }
+}
---
+++ 2019/SeleniumGroundUp/SeleniumDemo/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Remote;
+using OpenQA.Selenium.Firefox;
+using System.Linq;
+
+namespace SeleniumDemo
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var p = new Program();
+
+            p.FirstTest();
+        }
+
+        public void FirstTest()
+        {
+            RemoteWebDriver driver = null;
+            try
+            {
+                var driverOptions = new FirefoxOptions();
+                driverOptions.BrowserExecutableLocation = "";
+                driver = new FirefoxDriver(driverOptions);
+
+                driver.Manage().Window.Maximize();
+
+                driver.Navigate().GoToUrl("http://www.github.com");
+
+                var home = new GithubHome(driver);
+
+                var results = home.Search("quake");
+
+                results.ClickResult(0);
+            }
+            finally
+            {
+                driver?.Close();
+            }
+        }
+    }
+}
